37a19b190e9fc2ca1a2b818929a41fd163e0651d
Diego-Catalan-cmd <dcatalan2025@alu.uct.cl>
2025-11-24 01:22:44 -0600

    fix index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; // <--- IMPORTANTE
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter> {/* <--- ENVOLVER LA APP */}
+    <HashRouter> {/* <--- ENVOLVER LA APP */}
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
